b2427874d9bda11319051500f8622242aec36023
Leo <gluharevleon@gmail.com>
2025-10-30 16:47:02 +0300

    feat: add task input form

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task Tracker App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const TaskScreen(),
+    );
+  }
+}
+
+class TaskScreen extends StatelessWidget {
+  const TaskScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Мои задачи')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Задача добавлена!')),
+            );
+          },
+          child: const Text('Добавить задачу'),
+        ),
+      ),
+    );
+  }
+}
